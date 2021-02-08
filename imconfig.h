90acee4883a56f22992d2a86e0c7c7568d277a82
Mehdi Benadel <mehdi.benadel@inria.fr>
2021-02-08 15:49:39 +0100

    Linux export fix

--- imconfig.h
+++ imconfig.h
@@ -14,13 +14,23 @@
 
 //---- Define attributes of all API symbols declarations, e.g. for DLL under Windows
 // Using dear imgui via a shared library is not recommended, because of function call overhead and because we don't guarantee backward nor forward ABI compatibility.
-#ifdef IMGUI_EXPORTS
-    /* We are building this library */
-#  define IMGUI_API __declspec(dllexport)
-#else
-    /* We are using this library */
-#  define IMGUI_API __declspec(dllimport)
-#endif
+# if defined(_WIN32)
+#  ifdef IMGUI_STATIC_DEFINE
+#    define IMGUI_API
+#  else
+#    ifndef IMGUI_API
+#      ifdef IMGUI_EXPORTS
+/* We are building this library */
+#        define IMGUI_API __declspec(dllexport)
+#      else
+/* We are using this library */
+#        define IMGUI_API __declspec(dllimport)
+#      endif
+#    endif
+#  endif
+# else
+#  define IMGUI_API
+# endif
 
 //---- Don't define obsolete functions names. Consider enabling from time to time or when updating to reduce likelihood of using already obsolete function/names
 //#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
